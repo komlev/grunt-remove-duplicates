90a496a7fe24c9700b866812b76c4b3b7a82ecb5
Komlev <komlev88@gmail.com>
2014-11-03 17:07:06 +0300

    adding styles duplicate remover

--- tasks/remove_duplicates.js
+++ tasks/remove_duplicates.js
@@ -34,26 +34,50 @@ module.exports = function(grunt) {
       tmp,
       ind,
       map={},
-      dups = [],
-      srcs=[],
-      regexp = /\s*<script[^>]+src="?([^"\s]+)"?\s*>\s*<\/script>/gm;
-    while (match = regexp.exec(content)) {
-      srcs.push(match[1]);
+      scriptDups = [],
+      styleDups = [],
+      scriptSrc=[],
+      styleSrc=[],
+      scriptRegExp = /\s*<script[^>]+src="?([^"\s]+)"?\s*>\s*<\/script>/gm,
+      styleRegExp = /\s*<link[^>]+href="?([^"\s]+)"?.*\/?>/gm;
+
+    match = undefined;
+    while (match = scriptRegExp.exec(content)) {
+      scriptSrc.push(match[1]);
     }
 
-    _.each(srcs, function(item) {
+    while (match = styleRegExp.exec(content)) {
+      styleSrc.push(match[1]);
+    }
+
+    _.each(scriptSrc, function(item) {
       if (map[item] !== undefined) {
          map[item] = map[item] + 1;
-         if (dups.indexOf(item) === -1) {
-          dups.push(item);
+         if (scriptDups.indexOf(item) === -1) {
+          scriptDups.push(item);
          }
       } else {
         map[item] = 0;
       }
     });
-    for (var i = 0; i < dups.length; i++) {
-      dup = dups[i];
+
+    _.each(styleSrc, function(item) {
+      if (map[item] !== undefined) {
+         map[item] = map[item] + 1;
+         if (styleDups.indexOf(item) === -1) {
+          styleDups.push(item);
+         }
+      } else {
+        map[item] = 0;
+      }
+    });
+
+    for (var i = 0; i < scriptDups.length; i++) {
+      dup = scriptDups[i];
       tmp = 'REMOVE_DUPLICATES_PLACEHOLDER_' + i;
+
+      if (!dup)
+        continue;
 
       if (options.keepLast) {
         ind = content.lastIndexOf(dup);
@@ -61,10 +85,26 @@ module.exports = function(grunt) {
       } else {
         content = content.replace(dup, tmp);
       }
-      regexp = new RegExp('\\s*<script[^>]+src="' + dup + '"?\\s*>\\s*<\/script>', 'gm');
-      content = content.replace(regexp, '');
+      scriptRegExp = new RegExp('\\s*<script[^>]+src="' + dup + '"?\\s*>\\s*<\/script>', 'gm');
+      content = content.replace(scriptRegExp, '');
       content = content.replace(tmp, dup);
-      grunt.log.writeln('Duplications of "' + dup + '": ' + map[dup]);
+    }
+
+    for (var i = 0; i < styleDups.length; i++) {
+      dup = styleDups[i];
+      tmp = 'REMOVE_DUPLICATES_PLACEHOLDER_' + i;
+
+      if (!dup)
+        continue;
+      if (options.keepLast) {
+        ind = content.lastIndexOf(dup);
+        content = content.substr(0, ind) + tmp + content.substr(ind + dup.length);
+      } else {
+        content = content.replace(dup, tmp);
+      }
+      styleRegExp = new RegExp('\\s*<link[^>]+href="' + dup + '".*\/?>', 'gm');
+      content = content.replace(styleRegExp, '');
+      content = content.replace(tmp, dup);
     }
 
     return content;
